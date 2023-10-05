c11f58ca942203a7704ad1c565a1e4ddfd05e6c3
farhan0167 <afi225@nyu.edu>
2023-10-05 11:09:46 -0400

    ferry fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react'
 import Trains from './components/Trains';
 import Tram from './components/Tram';
+import Ferry from './components/Ferry';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -13,6 +14,7 @@ function App() {
       <Container style={{'marginTop': '50px'}}>
         <Row>
           <Col sm={12} md={12} lg={4}><Trains serverEndpoint={serverEndpoint}/></Col>
+          <Col sm={12} md={12} lg={4}><Ferry serverEndpoint={serverEndpoint}/></Col>
           <Col sm={12} md={12} lg={4}><Tram serverEndpoint={serverEndpoint}/></Col>
         </Row>
       </Container>
